e63bf7024d0fc2b0d89ef884f5ccfef72514e821
David French <david@davidfrench.io>
2026-03-08 13:42:56 -0400

    Fix root redirect - send to dashboard.html instead of automation rules

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <h1>Aegis Dashboard</h1>
-      <p>Redirecting to automation rules...</p>
-      <script dangerouslySetInnerHTML={{__html: `window.location.href='/app/apex/automation'`}} />
+      <p>Redirecting...</p>
+      <script dangerouslySetInnerHTML={{__html: `window.location.href='/dashboard.html'`}} />
     </main>
   )
 }
